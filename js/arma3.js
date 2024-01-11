e48868e5d2107679111c5469eaa491710914dc38
OshidaBCF <swann.sanchez9@outlook.fr>
2024-01-11 22:22:33 +0100

    Update arma3.js

--- js/arma3.js
+++ js/arma3.js
@@ -5,63 +5,63 @@ function parseArmaModpackPreset()
 }
 
 function waitForElm(selector) {
-	return new Promise(resolve => {
-		if (document.querySelector(selector)) {
-			return resolve(document.querySelector(selector));
-		}
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
 
-		const observer = new MutationObserver(mutations => {
-			if (document.querySelector(selector)) {
-				resolve(document.querySelector(selector));
-				observer.disconnect();
-			}
-		});
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
 
-		observer.observe(document.body, {
-			childList: true,
-			subtree: true
-		});
-	});
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
 }
 
-waitForElm("#body > button")
-uploadInput = document.getElementById("armaModpackFile")
+waitForElm("#body > button").then((elm) => {
+    uploadInput = document.getElementById("armaModpackFile")
+	uploadInput.addEventListener(
+	"change",
+	() => {
+		// Calculate total size
+		let numberOfBytes = 0;
+		for (const file of uploadInput.files) {
+		numberOfBytes += file.size;
+		}
 
-uploadInput.addEventListener(
-"change",
-() => {
-	// Calculate total size
-	let numberOfBytes = 0;
-	for (const file of uploadInput.files) {
-	numberOfBytes += file.size;
-	}
+		// Approximate to the closest prefixed unit
+		const units = [
+		"B",
+		"KiB",
+		"MiB",
+		"GiB",
+		"TiB",
+		"PiB",
+		"EiB",
+		"ZiB",
+		"YiB",
+		];
+		const exponent = Math.min(
+		Math.floor(Math.log(numberOfBytes) / Math.log(1024)),
+		units.length - 1,
+		);
+		const approx = numberOfBytes / 1024 ** exponent;
+		const output =
+		exponent === 0
+			? `${numberOfBytes} bytes`
+			: `${approx.toFixed(3)} ${
+				units[exponent]
+			} (${numberOfBytes} bytes)`;
 
-	// Approximate to the closest prefixed unit
-	const units = [
-	"B",
-	"KiB",
-	"MiB",
-	"GiB",
-	"TiB",
-	"PiB",
-	"EiB",
-	"ZiB",
-	"YiB",
-	];
-	const exponent = Math.min(
-	Math.floor(Math.log(numberOfBytes) / Math.log(1024)),
-	units.length - 1,
+		document.getElementById("fileNum").textContent = uploadInput.files.length;
+		document.getElementById("fileSize").textContent = output;
+	},
+	false,
 	);
-	const approx = numberOfBytes / 1024 ** exponent;
-	const output =
-	exponent === 0
-		? `${numberOfBytes} bytes`
-		: `${approx.toFixed(3)} ${
-			units[exponent]
-		} (${numberOfBytes} bytes)`;
-
-	document.getElementById("fileNum").textContent = uploadInput.files.length;
-	document.getElementById("fileSize").textContent = output;
-},
-false,
-);
+});
